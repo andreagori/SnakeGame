d9d9703af005136266d809e231237b01f30d5373
tessarivas <rivas.gomez.teresa@gmail.com>
2023-12-07 19:51:18 -0800

    cambio en mensajes

--- src/main.cpp
+++ src/main.cpp
@@ -1,97 +1,85 @@
-/*******************************************************************************************
- *
- *   raylib [core] examples - basic screen manager
- *
- *   NOTE: This example illustrates a very simple screen manager based on a states machines
- *
- *   Example originally created with raylib 4.0, last time updated with raylib 4.0
- *
- *   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
- *   BSD-like license that allows static linking with closed source software
- *
- *   Copyright (c) 2021-2023 Ramon Santamaria (@raysan5)
- *
- ********************************************************************************************/
-
 #include "raylib.h"
+#include <cmath>
+// #include "raymath.h"
 
-//------------------------------------------------------------------------------------------
-// Types and Structures Definition
-//------------------------------------------------------------------------------------------
 typedef enum GameScreen
 {
     INICIO = 0,
     JUGAR,
-    OPCIONES,
     CREDITOS
 } GameScreen;
 
-#define screenWidth 800
-#define screenHeight 450
+// SACAR LA RESOLUCION DEL MONITOR
+int screenWidth = GetMonitorWidth(0);
+int screenHeight = GetMonitorHeight(0);
 
 Texture2D backgroundTexture;
 Sound buttonSound;
+Texture2D buttonTextureA;
+Texture2D buttonTextureB;
+Music musica_fondo;
+
 void LoadContent();
 void UnloadContent();
+int drawinicio();
+void drawjugar();
+void drawopciones();
+void drawcreditos();
 void menudraw(GameScreen currentScreen);
+void ToggleFullscreenAndResize();
+bool musica(bool musicToggle, bool musicPaused);
 
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
+/* --------------------------------------------------------------------------------------------------- */
+/* PROGRAMA PRINCIPAL */
+/* --------------------------------------------------------------------------------------------------- */
 int main(void)
 {
-    // Initialization
-    //--------------------------------------------------------------------------------------
+    /* INICIALIZACION */
     InitWindow(screenWidth, screenHeight, "SIGN MATCH - GAME");
     InitAudioDevice();
 
     GameScreen currentScreen = INICIO;
 
-    // TODO: Initialize all required variables and load all required data here!
+    /* INICIALIZAR TODAS LAS VARIABLES REQUERIDAS PARA CARGAR LOS DATOS AQUI */
     LoadContent();
-    SetTargetFPS(60); // Set desired framerate (frames-per-second)
+    SetTargetFPS(60); // FRAMES POR SEGUNDO
+    bool musicPaused = false;
+    bool musicToggle = false;
 
-    Image icon = LoadImage("resources/signmatchicon.png");
+    Image icon = LoadImage("resources/SG_ICONO.png");
     SetWindowIcon(icon);
+    PlayMusicStream(musica_fondo);
 
-    //--------------------------------------------------------------------------------------
-
-    // Main game loop
-    while (!WindowShouldClose()) // Detect window close button or ESC key
+    /* ----------------------------------------------------------------------------------------------- */
+    /* LOOP PRINCIPAL DEL JUEGO */
+    /* ----------------------------------------------------------------------------------------------- */
+    while (!WindowShouldClose()) // DETECTAR QUE SE CIERRA EL PROGRAMA CON LA TECLA (ESC)
     {
-        // Update
-        //----------------------------------------------------------------------------------
+        ToggleFullscreenAndResize(); // MANEJAR CAMBIOS DE PANTALLA COMPLETA Y TAMAÑO DE VENTANA
+
+        int buttonClicked = drawinicio();
         switch (currentScreen)
         {
         case INICIO: // AQUI EN ESTA OPCION ESTARA EL MENU.
         {
-            if (IsKeyPressed(KEY_A))
+            if (buttonClicked == 1)
             {
                 PlaySound(buttonSound);
                 currentScreen = JUGAR;
             }
-            if (IsKeyPressed(KEY_S))
-            {
-                PlaySound(buttonSound);
-                currentScreen = OPCIONES;
-            }
-            if (IsKeyPressed(KEY_D))
+            if (buttonClicked == 2)
             {
                 PlaySound(buttonSound);
                 currentScreen = CREDITOS;
             }
-        }
-        break;
-        case JUGAR:
-        {
-            if (IsKeyPressed(KEY_DELETE))
+            if (IsKeyPressed(KEY_M))
             {
-                PlaySound(buttonSound);
-                currentScreen = INICIO;
+                musicToggle = !musicToggle; // CAMBIAR EL ESTADO DE LA MUSICA CON LA TECLA (M)
+                musicPaused = musica(musicToggle, musicPaused);
             }
         }
         break;
-        case OPCIONES:
+        case JUGAR:
         {
             if (IsKeyPressed(KEY_DELETE))
             {
@@ -112,35 +100,149 @@ int main(void)
         default:
             break;
         }
-        // Draw
-        //----------------------------------------------------------------------------------
+        /* ------------------------------------------------------------------------------------------- */
+        /* DIBUJAR */
+        /* ------------------------------------------------------------------------------------------- */
         menudraw(currentScreen);
+        UpdateMusicStream(musica_fondo);
     }
 
-    // TODO: Unload all loaded data (textures, fonts, audio) here!
+    /* ----------------------------------------------------------------------------------------------- */
+    /* CARGAR TODOS LOS DATOS AQUI: Texturas, Fuentes, Audio, etc... */
+    /* ----------------------------------------------------------------------------------------------- */
     UnloadContent();
     UnloadImage(icon);
 
-    CloseWindow(); // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
+    CloseWindow(); // CERRAR VENTANA
 
     return 0;
 }
 
+/* --------------------------------------------------------------------------------------------------- */
+
 void LoadContent()
 {
-    // Cargar la textura de fondo en el main
-    backgroundTexture = LoadTexture("resources/signmatch-MENU.png");
-    // AQUI PUEDO AGREGAR MAS IMAGENES PARA EL MENU, NO OLVIDAR AGREGARLAS EN EL UNLOADCONTENT.
+    /* CARGAR LA TEXTURA DE FONDO EN EL MAIN */
+    backgroundTexture = LoadTexture("resources/fullback.png");
+    /* AJUSTAR EL TAMAÑO DE LAS IMAGENES SEGUN LA RESOLUCION DE LA PANTALLA */
+    buttonTextureA = LoadTexture("resources/SM_BOTON.png");
+    buttonTextureB = LoadTexture("resources/SM_BOTONcred.png");
     buttonSound = LoadSound("audio/resources/buttonsound.wav");
+    musica_fondo = LoadMusicStream("audio/resources/fondo.mp3");
+    SetMusicVolume(musica_fondo, 1.0f);
 }
+
+/* --------------------------------------------------------------------------------------------------- */
+
+bool musica(bool musicToggle, bool musicPaused)
+{
+    if (musicToggle)
+    {
+        /* SI SE CUMPLE musicToggle, PAUSAR LA MUSICA SI ES QUE NO ESTA YA PAUSADA */
+        if (!musicPaused)
+        {
+            PauseMusicStream(musica_fondo);
+            musicPaused = true;
+        }
+    }
+    else
+    {
+        /* REANUDAR LA MUSICA SI musicToggle ESTA PAUSADA */
+        if (musicPaused)
+        {
+            ResumeMusicStream(musica_fondo);
+            musicPaused = false;
+        }
+    }
+    return musicPaused;
+}
+
+/* --------------------------------------------------------------------------------------------------- */
 
 void UnloadContent()
 {
-    // Liberar la textura de fondo al final del programa
+    /* LIBERAR LA TEXTURA DE FONDO AL FINAL DEL PROGRAMA */
     UnloadTexture(backgroundTexture);
     UnloadSound(buttonSound);
+    UnloadTexture(buttonTextureA);
+    UnloadTexture(buttonTextureB);
+    UnloadMusicStream(musica_fondo);
 }
+
+/* --------------------------------------------------------------------------------------------------- */
+
+int drawinicio()
+{
+    DrawTexturePro(
+        backgroundTexture,
+        (Rectangle){0, 0, (float)backgroundTexture.width, (float)backgroundTexture.height},
+        (Rectangle){0, 0, (float)GetScreenWidth(), (float)GetScreenHeight()},
+        (Vector2){0, 0},
+        0.0f,
+        WHITE);
+
+    /* DIBUJAR LOS BOTONES */
+    Rectangle buttonRectA = {585.0f, 320.0, (float)buttonTextureA.width, (float)buttonTextureA.height};
+    Rectangle buttonRectB = {585.0f, 420.0f, (float)buttonTextureB.width, (float)buttonTextureB.height};
+
+    /* VERIFICAR SI EL MOUSE ESTA SOBRE LOS BOTONES */
+    bool isMouseOverButtonA = CheckCollisionPointRec(GetMousePosition(), buttonRectA);
+    bool isMouseOverButtonB = CheckCollisionPointRec(GetMousePosition(), buttonRectB);
+
+    /* CAMBIAR EL TAMAÑO DEL BOTON (A) SI EL MOUSE ESTA SOBRE DE EL */
+    if (isMouseOverButtonA)
+    {
+        /* AGRANDAR Y REDUCIR (A) */
+        float scale = 1.0f + 0.05f * sin(2.0f * GetTime());
+        buttonRectA.width = buttonTextureA.width * scale;
+        buttonRectA.height = buttonTextureA.height * scale;
+    }
+
+    if (isMouseOverButtonB)
+    {
+        /* AGRANDAR Y REDUCIR (B) */
+        float scale = 1.0f + 0.05f * sin(2.0f * GetTime());
+        buttonRectB.width = buttonTextureB.width * scale;
+        buttonRectB.height = buttonTextureB.height * scale;
+    }
+
+    /* AJUSTAR LA POSICION DEL BOTON PARA QUE ESTE EN EL CENTRO */
+    Vector2 buttonPosition = {buttonRectA.x + buttonRectA.width / 2, buttonRectA.y + buttonRectA.height / 2};
+    Vector2 buttonPositionB = {buttonRectB.x + buttonRectB.width / 2, buttonRectB.y + buttonRectB.height / 2};
+    DrawTexturePro(buttonTextureA, (Rectangle){0.0f, 0.0f, static_cast<float>(buttonTextureA.width), static_cast<float>(buttonTextureA.height)}, (Rectangle){buttonPosition.x, buttonPosition.y, buttonRectA.width, buttonRectA.height}, (Vector2){buttonRectA.width / 2, buttonRectA.height / 2}, 0.0f, WHITE);
+    DrawTexturePro(buttonTextureB, (Rectangle){0.0f, 0.0f, static_cast<float>(buttonTextureB.width), static_cast<float>(buttonTextureB.height)}, (Rectangle){buttonPositionB.x, buttonPositionB.y, buttonRectB.width, buttonRectB.height}, (Vector2){buttonRectB.width / 2, buttonRectB.height / 2}, 0.0f, WHITE);
+
+    /* VERIFICAR SI SE HIZO CLICK EN EL BOTON */
+    if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+    {
+        if (isMouseOverButtonA)
+            return 1; // Botón A clickeado
+        if (isMouseOverButtonB)
+            return 2; // Botón B clickeado
+    }
+
+    return 0; // Ningún botón
+}
+
+/* --------------------------------------------------------------------------------------------------- */
+
+void drawjugar()
+{
+    DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(), GREEN);
+    DrawText("JUGAR SCREEN", 20, 20, 40, DARKGREEN);
+    DrawText("PRESS DELETE to RETURN to INICIO SCREEN", 120, 280, 20, DARKGREEN);
+}
+
+/* --------------------------------------------------------------------------------------------------- */
+
+void drawcreditos()
+{
+    DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(), BLUE);
+    DrawText("CREDITOS SCREEN", 20, 20, 40, DARKBLUE);
+    DrawText("PRESS DELETE to RETURN to INICIO SCREEN", 120, 280, 20, DARKBLUE);
+}
+
+/* --------------------------------------------------------------------------------------------------- */
 
 void menudraw(GameScreen currentScreen)
 {
@@ -150,31 +252,44 @@ void menudraw(GameScreen currentScreen)
     switch (currentScreen)
     {
     case INICIO:
-        DrawTexture(backgroundTexture, 0, 0, WHITE);
-        DrawText("INICIO SCREEN", 20, 20, 40, LIGHTGRAY);
-        DrawText("PRESS A to JUMP to JUGAR SCREEN", 120, 220, 20, YELLOW);
-        DrawText("PRESS S to JUMP to OPCIONES SCREEN", 120, 240, 20, YELLOW);
-        DrawText("PRESS D to JUMP to CREDITOS SCREEN", 120, 260, 20, YELLOW);
-        DrawText("PRESS ESCAPE to EXIT", 120, 280, 20, YELLOW);
+        drawinicio();
         break;
     case JUGAR:
-        DrawRectangle(0, 0, screenWidth, screenHeight, GREEN);
-        DrawText("JUGAR SCREEN", 20, 20, 40, DARKGREEN);
-        DrawText("PRESS DELETE to RETURN to INICIO SCREEN", 120, 280, 20, DARKGREEN);
-        break;
-    case OPCIONES:
-        DrawRectangle(0, 0, screenWidth, screenHeight, PURPLE);
-        DrawText("OPCIONES SCREEN", 20, 20, 40, MAROON);
-        DrawText("PRESS DELETE to RETURN to INICIO SCREEN", 120, 280, 20, MAROON);
+        drawjugar();
         break;
     case CREDITOS:
-        DrawRectangle(0, 0, screenWidth, screenHeight, BLUE);
-        DrawText("CREDITOS SCREEN", 20, 20, 40, DARKBLUE);
-        DrawText("PRESS DELETE to RETURN to INICIO SCREEN", 120, 280, 20, DARKBLUE);
+        drawcreditos();
         break;
     default:
         break;
     }
 
     EndDrawing();
+}
+
+/* --------------------------------------------------------------------------------------------------- */
+
+void ToggleFullscreenAndResize()
+{
+    if (IsKeyPressed(KEY_F))
+    {
+        /* CAMBIAR LA PANTALLA COMPLETA AL PRESIONAR (F11) */
+        ToggleFullscreen();
+
+        /* AJUSTAR EL TAMAÑO DE LA PANTALLA AL CAMBIARLO A FULLSCREEN */
+        if (IsWindowFullscreen())
+        {
+            screenWidth = GetMonitorWidth(0);
+            screenHeight = GetMonitorHeight(0);
+        }
+        else
+        {
+            /* RESTAURAR LA RESOLUCION AL SALIR DE LA PANTALLA COMPLETA */
+            screenWidth = GetMonitorWidth(0);
+            screenHeight = GetMonitorHeight(0);
+        }
+
+        /* CAMBIAR EL TAMAÑO DE LA VENTANA */
+        SetWindowSize(screenWidth, screenHeight);
+    }
 }
