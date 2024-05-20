57a9c42d09d60203effb1c820a425eb2e05efc0d
andreagori <andrearivassg@gmail.com>
2024-05-19 17:09:04 -0700

    final changes done.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# VIBORITAZ
-Snake Game using *Linked Lists* in C.
+# Universidad Autonoma de Baja California: School Project
+### Paradigms of Programming (Paradigmas de Programación)
+### Software Engineering (Ingeniería de Software). Semester IV.
+---
+### Snake Game: VIBORITAZ
+>Snake Game using *Linked Lists* in C.
+>
+> Library used: raylib.h
+
+---
+### Instructions:
+1. Download the repository.
+2. Install the library raylib.h
+3. For some people is necesary to install the compiler "w64devkit" to run the game. You need to install it in the same folder where you have the raylib library installed.
+4. Run the game using the command `make` in the terminal. This will generate the executable file.
+5. Run the game using the command `./Game.exe` in the terminal.
+6. Enjoy :)
--- src/main.cpp
+++ src/main.cpp
@@ -90,6 +90,7 @@ int main(void)
             }
             else
             {
+                // logic if has eaten the apple and regenerate it.
                 if (has_eaten_food(&snake, &apple, 15, 15))
                 {
                     grow_snake(&snake, 15, 15);
@@ -99,15 +100,14 @@ int main(void)
             }
             break;
         }
-            // Update
-            //----------------------------------------------------------------------------------
         }
-        // Dibujar la pantalla actual
+        // Draw the current screen.
         if (current_screen == MENU)
         {
             draw_menu(assets, current_screen);
         }
     }
+    // Unload the assets and memory.
     unload(assets);
     UnloadImage(icon);
     UnloadTexture(apple.appledrawing);
@@ -119,59 +119,82 @@ int main(void)
     return 0;
 }
 
+// FUNCTIONS DEFINITIONS.
+/* Function to load the assets
+ * Parameters:
+ * assets (files) - the assets to load.
+ * Return:
+ * assets (files) - the assets loaded.
+ */
 files load(files assets)
 {
     assets.background = LoadTexture("resources/UABC_ANDREA_FONDO.png");
     return assets;
 }
 
+// Function to unload the assets
+/* Parameters:
+ * assets (files) - the assets to unload.
+ */
 void unload(files assets)
 {
     UnloadTexture(assets.background);
 }
 
+// Function to animate the text
+/* Parameters:
+ * texto (const char *) - the text to animate.
+ * fontSize (int) - the size of the font.
+ * textWidth (int) - the width of the text.
+ * textHeight (int) - the height of the text.
+ * assets (files) - the assets to use.
+ */
 void animetext(const char *texto, int fontSize, int textWidth, int textHeight, files assets)
 {
     static int timer = 0;
-
-    // Incrementar el temporizador
+    // Add the timer to the text.
     timer++;
 
-    // Obtener la posición del cursor
+    // Get the mouse position.
     Vector2 mousePosition = GetMousePosition();
 
-    // Crear un rectángulo del área ocupada por el texto
+    // Create a rectangle for the text.
     Rectangle textRect = {
-        (GetScreenWidth() - textWidth) / 2,   // posición x del texto
-        (GetScreenHeight() - textHeight) / 2, // posición y del texto
-        textWidth,                            // ancho del texto
-        textHeight                            // height del texto
+        (GetScreenWidth() - textWidth) / 2,   // x position of the text
+        (GetScreenHeight() - textHeight) / 2, // y position of the text
+        textWidth,                            // text width
+        textHeight                            // text height
     };
 
-    // Verificar si el cursor está sobre el área ocupada por el texto
+    // Check if the mouse is over the text.
     bool isMouseOverText = CheckCollisionPointRec(mousePosition, textRect);
 
-    // Si el cursor está sobre el texto, no aplicar animación
+    // If the mouse is over the text, don't animate it.
     if (isMouseOverText)
     {
         DrawText(texto, (GetScreenWidth() - textWidth) / 2, (GetScreenHeight() - textHeight) / 2, fontSize, YELLOW);
     }
     else
     {
-        // Si el cursor no está sobre el texto, aplicar animación
+        // if the mouse is not over the text, animate it.
         float scale = 1.0f + 0.05f * sin(2.0f * GetTime());
 
-        // Aplicar la escala al tamaño del texto
+        // aplicate the scale to the font size
         int newFontSize = fontSize * scale;
 
-        // Dibujar el texto con el nuevo tamaño
+        // draw the text with the new font size
         DrawText(texto, (GetScreenWidth() - textWidth * scale) / 2, (GetScreenHeight() - textHeight * scale) / 2, newFontSize, YELLOW);
     }
 }
 
+// Function to draw the grid
+/* Parameters:
+ * height (int) - the height of the grid.
+ * weight (int) - the weight of the grid.
+ */
 void grid(int height, int weight)
 {
-    // Dibuja el grid de fondo gris.
+    // Draw the grid lines on the screen.
     for (int i = 0; i < weight; i += 20)
     {
         DrawLine(i, 0, i, height, Fade(LIGHTGRAY, 0.1f));
@@ -182,6 +205,11 @@ void grid(int height, int weight)
     }
 }
 
+// Function to draw the menu
+/* Parameters:
+ * assets (files) - the assets to use.
+ * pantalla (GameScreen) - the screen to draw.
+ */
 void draw_menu(files assets, GameScreen pantalla)
 {
     BeginDrawing();
@@ -193,11 +221,10 @@ void draw_menu(files assets, GameScreen pantalla)
         0.0f,
         WHITE);
 
-    // DrawText("Press ENTER to start", 50, 250, 30, YELLOW);
     animetext("Press ENTER to start", 25, 250, 20, assets);
     DrawText("made by: @andreagori", 80, 380, 10, BLACK);
 
-    // Dibuja el grid de fondo gris.
+    // Draw the grid
     grid(450, 450);
 
     EndDrawing();
@@ -205,8 +232,7 @@ void draw_menu(files assets, GameScreen pantalla)
 
 void gridSnake(int height_cellsnumber, int width_cellsnumber, int gridHeight, int gridWidth)
 {
-    // Implement the logic for gridSnake function.
-    // For customization in color, check raylib library or use: CLITERAL(Color){ x, x, x, x }
+    // For customization in color,  use: CLITERAL(Color){ x, x, x, x }
     // Before everthing, we need to make sure the grid and rectangles will be in the middle.
     // Calculate the starting point to draw the grid, in the middle.
     int start_x = (screenWidth - gridWidth) / 2;
@@ -231,35 +257,33 @@ void gridSnake(int height_cellsnumber, int width_cellsnumber, int gridHeight, in
     }
 }
 
+// Function to draw the game
+/* Parameters:
+ * snake (Snake *) - the snake to draw.
+ * apple (Apple *) - the apple to draw.
+ * start_x (int) - the x position to start drawing.
+ * start_y (int) - the y position to start drawing.
+ */
 void draw_game(Snake *snake, Apple *apple, int start_x, int start_y)
 {
     BeginDrawing();
     ClearBackground(CLITERAL(Color){2, 43, 15, 255});
-    DrawText("[TAB] para volver al menu", 10, 10, 15, YELLOW);
+    DrawText("[TAB] to go back to the menu", 10, 10, 15, YELLOW);
 
-    // Dibuja el grid de fondo gris.
+    // Draw the grid background and the snake grid.
     grid(450, 450);
     gridSnake(15, 15, 360, 360);
 
-    // Dibuja la serpiente y la manzana
     int cellWidth = 360 / 15; // Assuming a 15x15 grid
     int cellHeight = 360 / 15;
 
+    // Draw the snake and the apple.
     direction_snake(snake, 15, 15, 360, 360);
     draw_apple(apple, snake->head, 15, 15, 360, 360, cellHeight, cellWidth, start_x, start_y);
 
     // Draw the score
     char scoreStr[50];
     sprintf(scoreStr, "Score: %i", snake->size);
-    // Dibuja el puntaje
     DrawText(scoreStr, 10, 430, 20, YELLOW);
     EndDrawing();
 }
-
-/*
-Dudas de documentacion:
-La palabra clave const se debe usar siempre que sea apropiado. En barr dice que es una gran alternativa al uso del define? (entonces no lo incluyo en el codigo?)
-En donde deberia de poner el static?
-La palabra clave static se debe usar para declarar todas las funciones y variables que no necesitan ser visibles fuera del módulo en el que
-se declaran.
-*/
